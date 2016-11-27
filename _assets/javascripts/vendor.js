ad3aacfb4626af18913e9932ee416ff1ccbd9a6d
Brett Terespolsky <brett.terespolsky@innovationswitch.com>
2016-11-27 21:59:49 +0200

    Moving working code to master branch.

---
+++ _assets/javascripts/vendor.js
@@ -0,0 +1,7 @@
+//= require jquery.js
+//= require bootstrap.js
+//= require validator.js
+//= require jquery.easing.min.js
+//= require jquery.fittext.js
+//= require wow.min.js
+//= require creative.js
